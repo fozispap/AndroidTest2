20a913f464e9ee16933944ba6d00f44a60b37ccc
Fotis Papadimitriou <fpapadimit@gmail.com>
2014-01-30 18:28:14 +0000

    Changes in TestGit2.java

---
+++ TestGit2/src/com/example/testgit2/TestGit2.java
@@ -0,0 +1,24 @@
+package com.example.testgit2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+
+public class TestGit2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		Log.d("filter", "example text");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
